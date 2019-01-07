285e72b6ee5da2c0c17991da72620c76a295578b
caigen <caigen@outlook.com>
2019-01-07 16:46:36 +0800

    add UDP receiver tool.

---
+++ UDPReceiver/UDPReceiver/UDPReceiver.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace UDPReceiver
+{
+    partial class UDPReceiver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CheckBoxAsync = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LabelCount = new System.Windows.Forms.Label();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBoxInterval = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxPort = new System.Windows.Forms.TextBox();
+            this.TextBoxIP = new System.Windows.Forms.TextBox();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.TextBoxData = new System.Windows.Forms.TextBox();
+            this.TimerReceive = new System.Windows.Forms.Timer(this.components);
+            this.TimerCheck = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // CheckBoxAsync
+            // 
+            this.CheckBoxAsync.AutoSize = true;
+            this.CheckBoxAsync.Location = new System.Drawing.Point(159, 236);
+            this.CheckBoxAsync.Name = "CheckBoxAsync";
+            this.CheckBoxAsync.Size = new System.Drawing.Size(54, 16);
+            this.CheckBoxAsync.TabIndex = 25;
+            this.CheckBoxAsync.Text = "Async";
+            this.CheckBoxAsync.UseVisualStyleBackColor = true;
+            this.CheckBoxAsync.CheckedChanged += new System.EventHandler(this.CheckBoxAsync_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Received:";
+            // 
+            // LabelCount
+            // 
+            this.LabelCount.AutoSize = true;
+            this.LabelCount.Location = new System.Drawing.Point(76, 241);
+            this.LabelCount.Name = "LabelCount";
+            this.LabelCount.Size = new System.Drawing.Size(11, 12);
+            this.LabelCount.TabIndex = 23;
+            this.LabelCount.Text = "0";
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(244, 204);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 22;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Data:";
+            // 
+            // TextBoxInterval
+            // 
+            this.TextBoxInterval.Location = new System.Drawing.Point(244, 24);
+            this.TextBoxInterval.Name = "TextBoxInterval";
+            this.TextBoxInterval.Size = new System.Drawing.Size(87, 21);
+            this.TextBoxInterval.TabIndex = 20;
+            this.TextBoxInterval.Text = "1000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Interval: (ms)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "IP:";
+            // 
+            // TextBoxPort
+            // 
+            this.TextBoxPort.Location = new System.Drawing.Point(159, 24);
+            this.TextBoxPort.Name = "TextBoxPort";
+            this.TextBoxPort.Size = new System.Drawing.Size(66, 21);
+            this.TextBoxPort.TabIndex = 16;
+            this.TextBoxPort.Text = "10111";
+            // 
+            // TextBoxIP
+            // 
+            this.TextBoxIP.Location = new System.Drawing.Point(11, 24);
+            this.TextBoxIP.Name = "TextBoxIP";
+            this.TextBoxIP.Size = new System.Drawing.Size(129, 21);
+            this.TextBoxIP.TabIndex = 15;
+            this.TextBoxIP.Text = "127.0.0.1";
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(9, 204);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 14;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // TextBoxData
+            // 
+            this.TextBoxData.Location = new System.Drawing.Point(9, 76);
+            this.TextBoxData.Multiline = true;
+            this.TextBoxData.Name = "TextBoxData";
+            this.TextBoxData.ReadOnly = true;
+            this.TextBoxData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxData.Size = new System.Drawing.Size(316, 113);
+            this.TextBoxData.TabIndex = 13;
+            this.TextBoxData.Text = "Hello World!";
+            // 
+            // TimerReceive
+            // 
+            this.TimerReceive.Tick += new System.EventHandler(this.TimerReceive_Tick);
+            // 
+            // TimerCheck
+            // 
+            this.TimerCheck.Enabled = true;
+            this.TimerCheck.Tick += new System.EventHandler(this.TimerCheck_Tick);
+            // 
+            // UDPReceiver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 262);
+            this.Controls.Add(this.CheckBoxAsync);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LabelCount);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TextBoxInterval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxPort);
+            this.Controls.Add(this.TextBoxIP);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.TextBoxData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "UDPReceiver";
+            this.Text = "UDPReceiver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox CheckBoxAsync;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LabelCount;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextBoxInterval;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxPort;
+        private System.Windows.Forms.TextBox TextBoxIP;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.TextBox TextBoxData;
+        private System.Windows.Forms.Timer TimerReceive;
+        private System.Windows.Forms.Timer TimerCheck;
+    }
+}
+
---
+++ UDPReceiver/UDPReceiver/UDPReceiver.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDPReceiver
+{
+    public partial class UDPReceiver : Form
+    {
+        private Socket socketUDP;
+        
+        private IPAddress ipAddress;
+        private int port;
+        private IPEndPoint ipEndPoint;
+
+        private int interval;
+        private byte[] dataBuffer;
+
+        private int count = 0;
+
+        // ReceiveFromAsync
+        private bool async = false;
+        private SocketAsyncEventArgs socketAsyncEventArgs;
+        private Mutex mutex = new Mutex();
+        private byte[] logicBuffer;
+        private int logicBufferUsedSize;
+        
+        public UDPReceiver()
+        {
+            InitializeComponent();
+        }
+
+        private void TimerReceive_Tick(object sender, EventArgs e)
+        {
+            if (!async)
+            {
+                // Poll first to avoid blocking the main thread.
+                if (socketUDP.Poll(1, SelectMode.SelectRead))
+                {
+                    EndPoint endPoint = (EndPoint)ipEndPoint;
+                    int size = socketUDP.ReceiveFrom(dataBuffer, ref endPoint);
+                    if (size > 0)
+                    {
+                        string data = Encoding.Default.GetString(dataBuffer);
+                        this.TextBoxData.AppendText(data);
+                        this.TextBoxData.AppendText("\r\n");
+                        this.TextBoxData.ScrollToCaret();
+
+                        ++count;
+                        this.LabelCount.Text = count.ToString();
+                    }
+                }
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socketUDP = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Dgram, ProtocolType.Udp);
+
+                ipAddress = IPAddress.Parse(this.TextBoxIP.Text);
+                port = int.Parse(this.TextBoxPort.Text);
+                ipEndPoint = new IPEndPoint(ipAddress, port);
+
+                // Receive needs Bind.
+                socketUDP.Bind(ipEndPoint);
+
+                interval = int.Parse(this.TextBoxInterval.Text);
+                dataBuffer = new byte[1024];
+                
+                count = 0;
+                this.LabelCount.Text = count.ToString();
+
+                this.TextBoxData.Clear();
+                this.ButtonStart.Enabled = false;
+
+                // ReceiveFromAsync
+                if (async)
+                {
+                    logicBuffer = new byte[2048];
+                    logicBufferUsedSize = 0;
+
+                    socketAsyncEventArgs = new SocketAsyncEventArgs();
+                    socketAsyncEventArgs.RemoteEndPoint = ipEndPoint;
+                    socketAsyncEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(AsyncCompleted);
+                    socketAsyncEventArgs.SetBuffer(dataBuffer, 0, dataBuffer.Length);
+
+                    this.TimerCheck.Interval = interval;
+
+                    TryReceive(socketAsyncEventArgs);
+                }
+                else
+                {
+                    this.TimerReceive.Enabled = true;
+                    this.TimerReceive.Interval = interval;
+                }
+            }
+            catch (Exception exception)
+            {
+                this.TimerReceive.Enabled = false;
+
+                this.TextBoxData.Text = exception.Message.ToString();
+            }
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            this.TimerReceive.Enabled = false;
+
+            // Bound socket must be closed before reuse the port.
+            socketUDP.Close();
+
+            this.ButtonStart.Enabled = true;
+        }
+
+        private void CheckBoxAsync_CheckedChanged(object sender, EventArgs e)
+        {
+            async = this.CheckBoxAsync.Checked;
+        }
+
+        private void TryReceive(SocketAsyncEventArgs e)
+        {
+            try
+            {
+                if (!socketUDP.ReceiveFromAsync(e))
+                {
+                    ProcessReceive(e);
+                }
+            }
+            catch (Exception)
+            {
+                // When socket is closed.
+            }
+        }
+
+        private void AsyncCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            switch (e.LastOperation)
+            {
+                case SocketAsyncOperation.ReceiveFrom:
+                    ProcessReceive(e);
+                    break;
+                default:
+                    break;                    
+            }
+
+            TryReceive(e);
+        }
+
+        private void ProcessReceive(SocketAsyncEventArgs e)
+        {
+            if (e.BytesTransferred > 0)
+            {
+                mutex.WaitOne();
+
+                ++count;
+
+                if (e.BytesTransferred < logicBuffer.Length - logicBufferUsedSize)
+                {
+                    System.Array.Copy(dataBuffer, 0, logicBuffer, logicBufferUsedSize, e.BytesTransferred);
+                    logicBufferUsedSize += e.BytesTransferred;
+                }
+
+                mutex.ReleaseMutex();
+            }
+        }
+
+        private void TimerCheck_Tick(object sender, EventArgs e)
+        {
+            mutex.WaitOne();
+
+            this.LabelCount.Text = count.ToString();
+
+            if (logicBufferUsedSize > 0)
+            {
+                string logicString = Encoding.Default.GetString(logicBuffer);
+
+                this.TextBoxData.AppendText(logicString);
+                this.TextBoxData.AppendText("\r\n");
+                this.TextBoxData.ScrollToCaret();
+
+                logicBufferUsedSize = 0;
+            }
+
+            mutex.ReleaseMutex();
+        }
+    }
+}
