92b654ee63240dc52d1e68e1aefeb439dd46bd5a
caigen <caigen@outlook.com>
2019-01-07 10:34:16 +0800

    UDPSender Tool.

---
+++ UDPSender/UDPSender/UDPSender.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace UDPSender
+{
+    partial class UDPSender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TextBoxData = new System.Windows.Forms.TextBox();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.TextBoxIP = new System.Windows.Forms.TextBox();
+            this.TextBoxPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimerSend = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxInterval = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.LabelCount = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBoxData
+            // 
+            this.TextBoxData.Location = new System.Drawing.Point(10, 76);
+            this.TextBoxData.Multiline = true;
+            this.TextBoxData.Name = "TextBoxData";
+            this.TextBoxData.Size = new System.Drawing.Size(316, 113);
+            this.TextBoxData.TabIndex = 0;
+            this.TextBoxData.Text = "Hello World!";
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(10, 204);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 1;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // TextBoxIP
+            // 
+            this.TextBoxIP.Location = new System.Drawing.Point(12, 24);
+            this.TextBoxIP.Name = "TextBoxIP";
+            this.TextBoxIP.Size = new System.Drawing.Size(129, 21);
+            this.TextBoxIP.TabIndex = 2;
+            this.TextBoxIP.Text = "127.0.0.1";
+            // 
+            // TextBoxPort
+            // 
+            this.TextBoxPort.Location = new System.Drawing.Point(160, 24);
+            this.TextBoxPort.Name = "TextBoxPort";
+            this.TextBoxPort.Size = new System.Drawing.Size(66, 21);
+            this.TextBoxPort.TabIndex = 3;
+            this.TextBoxPort.Text = "10111";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            // 
+            // TimerSend
+            // 
+            this.TimerSend.Tick += new System.EventHandler(this.TimerSend_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Interval: (ms)";
+            // 
+            // TextBoxInterval
+            // 
+            this.TextBoxInterval.Location = new System.Drawing.Point(245, 24);
+            this.TextBoxInterval.Name = "TextBoxInterval";
+            this.TextBoxInterval.Size = new System.Drawing.Size(87, 21);
+            this.TextBoxInterval.TabIndex = 7;
+            this.TextBoxInterval.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Data:";
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(245, 204);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 9;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // LabelCount
+            // 
+            this.LabelCount.AutoSize = true;
+            this.LabelCount.Location = new System.Drawing.Point(53, 241);
+            this.LabelCount.Name = "LabelCount";
+            this.LabelCount.Size = new System.Drawing.Size(11, 12);
+            this.LabelCount.TabIndex = 10;
+            this.LabelCount.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sent:";
+            // 
+            // UDPSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 262);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LabelCount);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TextBoxInterval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxPort);
+            this.Controls.Add(this.TextBoxIP);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.TextBoxData);
+            this.Name = "UDPSender";
+            this.Text = "UDPSender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxData;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.TextBox TextBoxIP;
+        private System.Windows.Forms.TextBox TextBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer TimerSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.TextBox TextBoxInterval;
+        private System.Windows.Forms.Label LabelCount;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ UDPSender/UDPSender/UDPSender.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPSender
+{
+    public partial class UDPSender : Form
+    {
+        private IPAddress ipAddress;
+        private int port;
+        private int interval;
+        private IPEndPoint ipEndPoint;
+
+        private Socket socketUDP;
+        private byte[] dataBuffer;
+        
+        public UDPSender()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ipAddress = IPAddress.Parse(this.TextBoxIP.Text);
+                port = int.Parse(this.TextBoxPort.Text);
+                interval = int.Parse(this.TextBoxInterval.Text);
+                ipEndPoint = new IPEndPoint(ipAddress, port);
+
+                socketUDP = new Socket(ipEndPoint.AddressFamily,
+                    SocketType.Dgram, ProtocolType.Udp);
+
+                dataBuffer = Encoding.Default.GetBytes(this.TextBoxData.Text);
+
+                this.TimerSend.Enabled = true;
+                this.TimerSend.Interval = interval;
+                this.LabelCount.Text = "0";
+            }
+            catch (Exception)
+            {
+                this.TimerSend.Enabled = false;
+            }
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            this.TimerSend.Enabled = false;
+        }
+
+        private void TimerSend_Tick(object sender, EventArgs e)
+        {
+            socketUDP.SendTo(dataBuffer, ipEndPoint);
+            int count = int.Parse(this.LabelCount.Text) + 1;
+            this.LabelCount.Text = count.ToString();
+        }
+    }
+}
